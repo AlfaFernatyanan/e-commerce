d51bad91bb75d9fd36ae7575d5e1cc429be0e1ec
AlfaFernatyanan <113615607+AlfaFernatyanan@users.noreply.github.com>
2023-07-29 00:24:39 +0700

    change title to dev mode

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -4,11 +4,11 @@ function Header() {
   return (
     <div className="header">
       <img src="src\assets\Menu.png" />
-      <h1 className="title">Alfa</h1>
+      <h1 className="title">Alfa (Dev version) </h1>
 
       <div className="login">
         <div>
-          Car
+          Cart
           <img className="small-icon" src="src\assets\cart.svg" />
         </div>
         Login
